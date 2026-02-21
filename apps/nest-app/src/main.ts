588831d800d8029e1048c7cad9e6b8cd855a2efb
Bobo5diallo224 <bobo77263.diallo@gmail.com>
2026-02-21 15:40:46 +0000

    chore: update pnpm-workspace.yaml to include onlyBuiltDependencies for better-sqlite3

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Backend API')
+    .setDescription('The backend API description')
+    .setVersion('1.0')
+    .addTag('backend-api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- apps/nest-app/src/users/entities/user.entity.ts
+++ apps/nest-app/src/users/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+@Entity()
+export class User {
+@PrimaryColumn({ type: 'uuid', generated: 'uuid' })
+id: string;
+
+@Column()
+name: string;
+
+@Column()
+email: string;
+}
