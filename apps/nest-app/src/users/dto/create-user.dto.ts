832dc32ea39283fcaa395682027bf1c53204b44f
Bobo5diallo224 <bobo77263.diallo@gmail.com>
2026-02-26 19:12:14 +0000

    update sur le chmpas email

--- apps/nest-app/src/users/dto/create-user.dto.ts
+++ apps/nest-app/src/users/dto/create-user.dto.ts
@@ -1,8 +1,9 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 export class CreateUserDto {
 @IsString()
 name: string;
 
 @IsEmail()
+@IsOptional()
 email: string;
 }
